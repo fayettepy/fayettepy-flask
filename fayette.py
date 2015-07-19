1e34070f3687af6d26bbf950e85aae9a4a84d15c
Casey Kinsey <casey.kinsey@gmail.com>
2015-07-19 14:17:12 -0500

    Better routing example

--- fayette.py
+++ fayette.py
@@ -11,13 +11,13 @@ def index():
     return "<h1>It worked!</h1>"
 
 
-@app.route("/other-page")
-def other_page():
+@app.route("/profile")
+def profile():
     """
     This is the index view for FayettePy.
     It is loaded at "/"
     """
-    return "<h1>This is another page!</h1>"
+    return "<h1>This is a user's profile page</h1>"
 
 if __name__ == '__main__':
     app.debug = True
