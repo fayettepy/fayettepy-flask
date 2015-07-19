8d0b41b66cac6876cf01aa9468faf5a2f9416585
Casey Kinsey <casey.kinsey@gmail.com>
2015-07-19 14:11:18 -0500

    Uses routing to implement a second page

--- fayette.py
+++ fayette.py
@@ -10,6 +10,15 @@ def index():
     """
     return "<h1>It worked!</h1>"
 
+
+@app.route("/other-page")
+def other_page():
+    """
+    This is the index view for FayettePy.
+    It is loaded at "/"
+    """
+    return "<h1>This is another page!</h1>"
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
