37577fbca988d2f08cf062d94b3c5943486943e5
Casey Kinsey <casey.kinsey@gmail.com>
2015-07-19 17:34:39 -0500

    Adds stubs for events/members views

--- fayette.py
+++ fayette.py
@@ -10,14 +10,21 @@ def index():
     """
     return render_template("index.html", color_list=['red', 'green', 'blue'])
 
+@app.route("/members")
+def members():
+    """
+    Load members via the Meetup API
+    """
+    members = []
+    return render_template("members.html", members=members)
 
 @app.route("/events")
 def events():
     """
-    This is the index view for FayettePy.
-    It is loaded at "/"
+    Load events via the Meetup API
     """
-    return render_template("events.html")
+    events = []
+    return render_template("events.html", events=events)
 
 if __name__ == '__main__':
     app.debug = True
